b50d1697805247bb57dfb0b45bd42e787a6c3273
Samuel Loza <starsaminf@gmail.com>
2020-12-03 10:01:37 -0400

    [Develop] Environment Trello (#8)
    * Add EnvironmentTrello
    
    * Update .gitignore file

---
+++ src/main/java/org/fundacionjala/trello/config/EnvironmentTrello.java
@@ -0,0 +1,47 @@
+package org.fundacionjala.trello.config;
+import org.fundacionjala.core.config.Environment;
+
+/**
+ * PropertiesReader Trello class.
+ */
+public final class EnvironmentTrello extends Environment {
+
+    private static final String GRADLE_FILE = "gradle.properties";
+    private static final String API_TOKEN = "token";
+    private static final String API_KEY = "key";
+    private static EnvironmentTrello environment;
+
+    /**
+     * Initialize an instance of the Environment class.
+     */
+    private EnvironmentTrello() {
+        super(GRADLE_FILE);
+    }
+
+    /**
+     * Gets a singleton instance of the EnvironmentTrello.
+     * @return EnvironmentTrello instance.
+     */
+    public static EnvironmentTrello getInstance() {
+        if (environment == null) {
+            environment = new EnvironmentTrello();
+        }
+        return environment;
+    }
+
+    /**
+     * Gets environment API Token.
+     * @return environment API Token value.
+     */
+    public String getEnvApiToken() {
+        return this.getEnvProperty(API_TOKEN);
+    }
+
+    /**
+     * Gets environment API Key.
+     * @return environment API Key value.
+     */
+    public String getEnvApiKey() {
+        return this.getEnvProperty(API_KEY);
+    }
+}
