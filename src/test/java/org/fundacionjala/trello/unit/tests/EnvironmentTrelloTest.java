aa830303d7e449cb2c4a4be282c50340753dbf26
Samuel Loza <starsaminf@gmail.com>
2020-12-03 09:12:46 -0400

    Add EnvironmentTrello

---
+++ src/test/java/org/fundacionjala/trello/unit/tests/EnvironmentTrelloTest.java
@@ -0,0 +1,26 @@
+package org.fundacionjala.trello.unit.tests;
+
+import org.fundacionjala.trello.config.EnvironmentTrello;
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+public class EnvironmentTrelloTest {
+
+    /**
+     * Test get Api Token from the properties file.
+     */
+    @Test
+    public void testGetEnvApiToken() {
+        EnvironmentTrello reader = EnvironmentTrello.getInstance();
+        Assert.assertNotNull(reader.getEnvApiToken());
+    }
+
+    /**
+     * Test get Api Key from the properties file.
+     */
+    @Test
+    public void testGetEnvApiKey() {
+        EnvironmentTrello reader = EnvironmentTrello.getInstance();
+        Assert.assertNotNull(reader.getEnvApiKey());
+    }
+}
